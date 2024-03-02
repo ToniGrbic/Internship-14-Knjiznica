99175efb75fc5d08faac28a5c54c9d4fe3589903
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-02 17:27:37 +0100

    filters container UI

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import SelectOptions from "./SelectOptions";
+import Button from "./Button";
+import { categories } from "./Form/Form";
 
 const Filter = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [seachCategory, setSearchCategory] = useState("");
+  const [searchCategory, setSearchCategory] = useState("All");
+
+  useEffect(() => {
+    console.log(searchTerm, searchCategory);
+  }, [searchTerm, searchCategory]);
   return (
-    <div>
-      <input type="text" placeholder="search by name and author" />
+    <div className="filtersContainer">
+      <div className="filterInputs">
+        <input
+          id="searchInput"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          type="text"
+          placeholder="search by name and author"
+        />
+        <select
+          id="searchCategory"
+          value={searchCategory}
+          onChange={(e) => setSearchCategory(e.target.value)}
+        >
+          <SelectOptions options={["All", ...categories]} />
+        </select>
+      </div>
+      <Button name="Search" />
     </div>
   );
 };

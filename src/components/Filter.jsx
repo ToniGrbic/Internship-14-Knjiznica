b27ea80168bb0c2236c67f89b0b1982f595777d2
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-03 15:41:38 +0100

    filter on text input and bug fix

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,34 +3,38 @@ import SelectOptions from "./SelectOptions";
 import ButtonSubmit from "./ButtonSubmit";
 import { categories } from "./Form/Form";
 
-const Filter = ({ books, filteredBooks, setFilteredBooks }) => {
+const Filter = ({ books, setFilteredBooks }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchCategory, setSearchCategory] = useState("All");
 
-  const filterBooks = (books) => {
+  const filterBooks = (books, searchTerm) => {
     return books.filter((book) => {
+      const category =
+        searchCategory === "All" ? true : book.category === searchCategory;
+      if (searchTerm === "") return category;
+
       const title = book.title.toLowerCase().includes(searchTerm.toLowerCase());
       const author = book.author
         .toLowerCase()
         .includes(searchTerm.toLowerCase());
-      const category =
-        searchCategory === "All" ? true : book.category === searchCategory;
-      if (searchTerm === "") return category;
+
       return (title || author) && category;
     });
   };
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    const filtered = filterBooks(books);
+    const filtered = filterBooks(books, e.target.value);
     setFilteredBooks(filtered);
   };
 
   const handleSearchTerm = (e) => {
-    if (e.target.value === "") {
-      setFilteredBooks(filterBooks(books));
-    }
     setSearchTerm(e.target.value);
+
+    setTimeout(() => {
+      const filtered = filterBooks(books, e.target.value);
+      setFilteredBooks(filtered);
+    }, 400);
   };
 
   return (

3aa392dd3177cfa78ee0e1a5db5a84b45904f47b
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-03 16:01:51 +0100

    red outline if input empty

--- src/components/Form/TextInput.jsx
+++ src/components/Form/TextInput.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TextInput = ({ name, value, field, setFormData }) => {
+  const [isInputEmpty, setIsInputEmpty] = useState(false);
   return (
     <div className="textInputDiv">
       <p className="textInputName">{name}</p>
       <input
+        className={isInputEmpty ? "inputEmpty" : ""}
         name="textInput"
         type="text"
         value={value}
-        onChange={(e) => setFormData({ [field]: e.target.value })}
+        onChange={(e) => {
+          if (e.target.value == "") setIsInputEmpty(true);
+          else setIsInputEmpty(false);
+          setFormData({ [field]: e.target.value });
+        }}
       />
     </div>
   );
